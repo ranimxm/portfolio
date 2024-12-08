636b33b717f1b3a8cc13effc7b0a6d237b83831d
Ranim <ranimm01@hotmail.com>
2024-12-08 13:32:36 +0100

    feat: change Blob component with changed shader effects and color transitions

--- src/components/blob.tsx
+++ src/components/blob.tsx
@@ -18,28 +18,43 @@ export default function Blob() {
         const clock = new THREE.Clock();
 
         const blobMaterial = new THREE.ShaderMaterial({
-            uniforms: {
-                uTime: { value: 0.0 },
-            },
-            vertexShader: `
+          uniforms: {
+            uTime: { value: 0.0 },
+          },
+          vertexShader: `
                 uniform float uTime;
                 varying vec2 vUv;
 
                 void main() {
-                vUv = uv;
-                vec3 newPosition = position;
+                    vUv = uv;
+                    vec3 newPosition = position;
 
-                float ripple = sin((position.x * 1.0 + position.y * 5.0) + uTime * 1.0) * 0.1;
-                newPosition.z += ripple;
+                    float ripple = sin((position.x * 5.0 + position.y * 5.0) + uTime * 1.0) * 0.06;
+                    newPosition.z += ripple;
 
-                gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
+                    gl_Position = projectionMatrix * modelViewMatrix * vec4(newPosition, 1.0);
                 }
             `,
-            fragmentShader: `
+          fragmentShader: `
                 varying vec2 vUv;
 
                 void main() {
-                gl_FragColor = vec4(0.5 + 0.5 * vUv.x, 0.5 + 0.5 * vUv.y, 1.0, 1.0);
+                    vec2 center = vec2(0.5, 0.5);
+                    float radius = 0.5; // straal van de cirkel
+                    float dist = distance(vUv, center) / radius;
+
+                    vec3 color;
+                    if (dist < 0.4641) { //eerste cirkel (middelste) naar tweede
+                        color = mix(vec3(0.70196, 0.64705, 1.0), vec3(0.95294, 0.74509, 1.0), dist / 0.4641); //mix kleuren mbv CSS percentages in figma (46,51%)
+                    } else if (dist < 0.6783) { //tweede cirkel naar derde
+                        color = mix(vec3(0.95294, 0.74509, 1.0), vec3(0.58823, 0.52156, 1.0), (dist - 0.4641) / (0.6783 - 0.4641));
+                    } else if (dist < 0.8521) { //derde cirkel naar vierde
+                        color = mix(vec3(0.58823, 0.52156, 1.0), vec3(0.23921, 0.2, 0.47058), (dist - 0.6783) / (0.8521 - 0.6783));
+                    } else if (dist <= 1.0) { //vierde cirkel maar vijfde
+                        color = mix(vec3(0.23921, 0.2, 0.47058), vec3(0.01176, 0.0, 0.22745), (dist - 0.8521) / (1.0 - 0.8521));
+                    }
+
+                    gl_FragColor = vec4(color, 1.0);
                 }
             `,
         });
