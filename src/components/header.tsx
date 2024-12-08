f8faa70961eb423cbe2e55063aec9d3a50c86df8
Ranim <ranimm01@hotmail.com>
2024-12-08 23:25:41 +0100

    feat: enhance header component with responsive navigation and menu toggle functionality

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,15 +1,53 @@
+"use client";
+
+import { useState, useEffect } from "react";
 import Link from "next/link";
 
 export default function Header() { 
-    return (
-      <header className="flex items-center justify-between p-4 px-8 text-h4  text-white">
-        <div className="flex flex-col items-start justify-center">
-          <p>Ranim</p>
-          <p>Mohammad</p>
+  const [navActive, setNavActive] = useState(false);
+  const [menuActive, setMenuActive] = useState(false);
+
+  const toggleMenu = () => {
+    setNavActive(!navActive);
+    setMenuActive(!menuActive);
+  };
+
+  useEffect(() => {
+    const navigation = document.querySelector(".nav") as HTMLElement;
+    const menu = document.querySelector(".menu");
+
+    menu?.addEventListener("click", () => {
+      navigation.style.setProperty("--childenNumber", navigation.children.length.toString());
+    });
+  }, []);
+
+  return (
+    <header className="flex items-center justify-between p-4 px-8 text-h4 text-white">
+      <div className="flex flex-col items-start justify-center">
+        <p>Ranim</p>
+        <p>Mohammad</p>
+      </div>
+      <nav className="z-[99]">
+        <ul
+          className={` nav nav-list flex flex-row gap-4 ${
+            navActive ? "active" : ""
+          }`}
+        >
+          <li className="p-2">
+            <Link href="/" className="hover-link" onClick={toggleMenu}> Home </Link>
+          </li>
+        </ul>
+        <div
+          className={`menu ${
+            menuActive ? "active" : ""
+          } hidden cursor-pointer relative flex-col items-center justify-between w-[23px] h-[18px] m-[20px]`}
+          onClick={toggleMenu}
+        >
+          <span className="w-full h-[2px] rounded-full bg-white transition origin-left duration-300 ease-linear"></span>
+          <span className="w-full h-[2px] rounded-full bg-white transition origin-left duration-300 ease-linear"></span>
+          <span className="w-full h-[2px] rounded-full bg-white transition origin-left duration-300 ease-linear"></span>
         </div>
-        <nav className="flex flex-row gap-4">
-          <Link className="hover-link" href={"/"}>home</Link>
-        </nav>
-      </header>
-    );
+      </nav>
+    </header>
+  );
 }
