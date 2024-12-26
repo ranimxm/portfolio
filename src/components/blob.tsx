da676d00e90c89fbd77dad745dd3ef3da397d882
ranim <ranimm01@hotmail.com>
2024-12-26 20:03:47 +0100

    removing unnecerssary background color scene

--- src/components/blob.tsx
+++ src/components/blob.tsx
@@ -10,9 +10,8 @@ export default function Blob() {
 
     useEffect(() => {
       const scene = new THREE.Scene();
-      scene.background = new THREE.Color(0x101010);
       const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-      const renderer = new THREE.WebGLRenderer({ antialias: true });
+      const renderer = new THREE.WebGLRenderer({ antialias: true, alpha: true });
       renderer.setSize(window.innerWidth, window.innerHeight);
 
       const container = containerRef.current;
