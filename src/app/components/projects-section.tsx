95e639cf6c272981c3dd2e9583ed32005bab4947
ranim <ranimm01@hotmail.com>
2024-12-28 13:57:26 +0100

    refactoring units to working ones

--- src/app/components/projects-section.tsx
+++ src/app/components/projects-section.tsx
@@ -3,26 +3,26 @@ import Blob from "@/components/blob";
 
 export default function ProjectsSection() {
     return (
-        <section className="section overflow-hidden w-full bg-white flex flex-col justify-center items-start border-borderLine border-t-2"
+        <section className="overflow-hidden w-full bg-white flex flex-col justify-center items-start border-borderLine border-t-2"
             style={{ clipPath: 'inset(0 0 0 0)' }}
         >
             <Blob />
-            <div className="px-[10dvw] w-full z-[10]">
+            <div className="px-[10vw] w-full z-[10]">
                 <section className="text-black h-full w-full flex flex-col">
-                    <div className="h-[70dvh] border-l-2 border-r-2  w-full border-borderLine flex flex-col text-h1 md:text-xxl h-[70dvh] flex flex-col justify-center items-start
-                    relative after:absolute after:content-[''] after:w-[300dvw] after:bottom-[0] after:h-[2px] after:left-[-200px] after:bg-borderLine"
+                    <div className="h-[70vh] border-l-2 border-r-2  w-full border-borderLine flex flex-col text-h1 md:text-xxl h-[70vh] flex flex-col justify-center items-start
+                    relative after:absolute after:content-[''] after:w-[300vw] after:bottom-[0] after:h-[2px] after:left-[-200px] after:bg-borderLine"
                     >
-                        <p className="p-[5dvw]">Projects</p>
+                        <p className="p-[5vw]">Projects</p>
                     </div>
                     <section className="border-borderLine border-r-2 border-l-2 w-full
-                    relative after:absolute after:content-[''] after:w-[300dvw] after:bottom-[0] after:h-[2px] after:left-[-200px] after:bg-borderLine
+                    relative after:absolute after:content-[''] after:w-[300vw] after:bottom-[0] after:h-[2px] after:left-[-200px] after:bg-borderLine
                     ">
                         <ol className="flex justify-center w-full flex-col">
-                            <li className="flex flex-col md:flex-row border-b-2 border-borderLine h-[90dvh] md:h-[50dvh] items-center gap-2 p-2 md:p-5">
-                                <figure className=" h-[50dvh] md:h-full md:w-[30%] w-full">
+                            <li className="flex flex-col md:flex-row border-b-2 border-borderLine h-[90vh] md:h-[50vh] items-center gap-2 p-2 md:p-5">
+                                <figure className=" h-[50vh] md:h-full md:w-[30%] w-full">
                                     <div className="h-full w-auto bg-mainTheme"></div>
                                 </figure>
-                                <figcaption className="md:p-[3em] grow-[1] px-[5dvw] text-h2 flex flex-col gap-2">
+                                <figcaption className="md:p-[3em] grow-[1] px-[5vw] text-h2 flex flex-col gap-2">
                                     <p>Lorem ipsum</p>
                                     <p>React Native, EJS, whatever</p>
                                 </figcaption>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import ProjectsSection from "./components/projects-section";
 export default function Home() {
   return (
     <>
-      <section className="h-[90dvh] w-full flex flex-col justify-center items-start px-[10dvw]">
+      <section className="h-[90vh] w-full flex flex-col justify-center items-start px-[10vw]">
         <div className="text-white w-full flex flex-col text-h1 md:text-xxl">
           <p>Ranim</p>
           <p>Mohammad</p>
