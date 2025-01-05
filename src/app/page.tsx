90ae23f16eeb3871f07a23a34e6b25998dee6995
ranim <ranimm01@hotmail.com>
2025-01-05 20:54:07 +0100

    add ArrowDown icon and scroll prompt to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { ArrowDown } from "@/assets/icons/arrowDown";
 import ProjectsSection from "./components/projects-section";
 
 export default function Home() {
@@ -5,8 +6,14 @@ export default function Home() {
     <>
       <section className="h-[90vh] w-full flex flex-col justify-center items-start px-[10vw]">
         <div className="text-white w-full flex flex-col text-h1 md:text-xxl">
-          <p>Ranim</p>
-          <p>Mohammad</p>
+          <div>
+            <p>Ranim</p>
+            <p>Mohammad</p>
+          </div>
+          <div className="md:text-h1 text-h3 flex gap-2 justify-end items-center">
+            <ArrowDown />
+            <p>scroll</p>
+          </div>
         </div>
       </section>
       <ProjectsSection />
