f8746faadc014129a81ae73bda1a13521423b8e3
ranim <ranimm01@hotmail.com>
2025-01-05 22:02:09 +0100

    implement dynamic project listing in ProjectsSection component

--- src/app/components/projects-section.tsx
+++ src/app/components/projects-section.tsx
@@ -1,6 +1,8 @@
 import { ChevronRight } from "@/assets/icons/chevronRight";
 import { AnimatedText } from "@/components/animated-text";
 import Blob from "@/components/blob";
+import projects from "@/data/projects.json";
+import Link from "next/link";
 
 export default function ProjectsSection() {
     return (
@@ -19,20 +21,25 @@ export default function ProjectsSection() {
                     relative after:absolute after:content-[''] after:w-[300vw] after:bottom-[0] after:h-[2px] after:left-[-200px] after:bg-borderLine
                     ">
                         <ol className="flex justify-center w-full flex-col">
-                            <li className="group hover:cursor-pointer flex flex-col md:flex-row border-b-2 border-borderLine h-[90vh] md:h-[50vh] items-center gap-2 p-2 md:p-5">
-                                <figure className=" h-[50vh] md:h-full md:w-[30%] w-full">
-                                    <div className="h-full w-auto bg-mainTheme"></div>
-                                </figure>
-                                <div className="flex flex-col md:flex-row w-[70%] items-center justify-between items-center">
-                                    <figcaption className="md:p-[3em] grow-[1] px-[5vw] text-h2 flex flex-col gap-8">
-                                        <AnimatedText text="Lorem ipsum" />
-                                        <AnimatedText text="React native, ejs whatever"/>
-                                    </figcaption>
-                                    <div className="px-5 md:block hidden">
-                                        <p> <ChevronRight className="text-black group-hover:text-mainTheme group-hover:-translate-x-4 ease-out duration-300"/> </p>
-                                    </div>
-                                </div>
-                            </li>
+                            {projects.map((project, index) => (
+                                <li key={project.id}>
+                                    <Link href={`/projects/${project.id}`} key={project.id}  className={`group hover:cursor-pointer flex flex-col md:flex-row 
+                                        ${index === projects.length -1 ? "border-b-0" : "border-b-2"} border-borderLine h-[90vh] md:h-[50vh] items-center gap-2 p-2 md:p-5`}>
+                                            <figure className=" h-[50vh] md:h-full md:w-[30%] w-full">
+                                            <div className="h-full w-auto bg-mainTheme"></div>
+                                        </figure>
+                                        <div className="flex flex-col md:flex-row w-[70%] items-center justify-between items-center">
+                                            <figcaption className="md:p-[3em] grow-[1] px-[5vw] text-h2 flex flex-col gap-8">
+                                                <AnimatedText text={project.title} />
+                                                <AnimatedText text={project.framework} />
+                                            </figcaption>
+                                            <div className="px-5 md:block hidden">
+                                                <ChevronRight className="text-black group-hover:text-mainTheme group-hover:-translate-x-4 ease-out duration-300"/>
+                                            </div>
+                                        </div>
+                                    </Link>
+                                </li>
+                            ))}
                         </ol>
                     </section>
                 </section>
