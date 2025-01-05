955b548b61304ad4dab4813827cfd0c529157a10
ranim <ranimm01@hotmail.com>
2025-01-05 22:34:49 +0100

    add ProjectPage component to display project details

---
+++ src/app/projects/[id]/page.tsx
@@ -0,0 +1,28 @@
+import Blob from "@/components/blob";
+import Header from "@/components/header";
+import projects from "@/data/projects.json";
+import { notFound } from "next/navigation";
+
+type ProjectPageProps = {
+  params: { id: string };
+}
+
+export default function ProjectPage({ params }: ProjectPageProps) {
+  const projectId = Number(params.id);
+  const project = projects.find((p) => p.id === projectId);
+
+  if (!project) {
+    return notFound(); 
+  }
+
+  return (
+    <>
+      <Header />
+      <main className="bg-white text-black h-[90vh] w-full">
+        <h1 className="text-xxl ">{project.title}</h1>
+        <p className="text-xl">{project.framework}</p>
+      </main>
+      <Blob />
+    </>
+  );
+}
