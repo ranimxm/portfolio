d607d087ca2f85a3ef853eff5cde7034dd72b927
ranim <ranimm01@hotmail.com>
2025-01-06 12:43:34 +0100

    refactor ProjectPage to enhance layout and add project descriptions

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import { AnimatedText } from "@/components/animated-text";
 import Blob from "@/components/blob";
 import Header from "@/components/header";
 import projects from "@/data/projects.json";
@@ -19,8 +20,21 @@ export default async function ProjectPage({ params }: ProjectPageProps) {
     <>
       <Header isLight />
       <main className="bg-white text-black h-[90vh] w-full" style={{ clipPath: 'inset(0 0 0 0)' }}>
-        <h1 className="text-xxl ">{project.title}</h1>
-        <p className="text-xl">{project.framework}</p>
+        <div className="px-[10vw] w-full z-[10]">
+          <section className="text-black h-full w-full flex flex-col">
+            <div className="p-[5vw] border-l-2 border-r-2  w-full border-borderLine flex flex-col text-h1 md:text-xxl h-[65vh] flex flex-col justify-center items-start
+            relative after:absolute after:content-[''] after:w-[300vw] after:bottom-[0] after:h-[2px] after:left-[-200px] after:bg-borderLine"
+            >
+              <h1 className="text-xxl"> {project.title} </h1>
+              <p className="text-xl">{project.framework}</p>
+            </div>
+            <section className="border-borderLine border-r-2 border-l-2 w-full">
+              <div className="flex justify-center w-full flex-col p-[5vw] w-[70%]">
+                <AnimatedText className="text-h3" text={project.description} />
+              </div>
+            </section>
+          </section>
+        </div>
         <Blob isLight />
       </main>
     </>
