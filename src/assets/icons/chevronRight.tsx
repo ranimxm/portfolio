00a18a196b90ed4824feaa179e4c083503ac2ef2
ranim <ranimm01@hotmail.com>
2025-01-07 21:12:42 +0100

    refactor ChevronRight icon to update size and improve styling

--- src/assets/icons/chevronRight.tsx
+++ src/assets/icons/chevronRight.tsx
@@ -1,20 +1,7 @@
 import { SvgProps } from "@/types/svg-props";
 
 export const ChevronRight = ({ className }: SvgProps) => (
-    <svg
-        width="40px"
-        height="40px"
-        viewBox="0 0 24 24"
-        fill="none"
-        xmlns="http://www.w3.org/2000/svg"
-        className={className}
-    >
-        <path
-        d="M8 5L15.57 11.6237C15.7976 11.8229 15.7976 12.1771 15.57 12.3763L8 19"
-        stroke="currentColor"
-        strokeWidth="2"
-        strokeLinecap="round"
-        strokeLinejoin="round"
-        />
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24" fill="none" className={className}>
+        <path d="M6.64615 3.31172L4.06731 2.9905e-07L5.53846 2.34743e-07L9 3.99002L5.53846 8L4.06731 8L6.62885 4.68828L-1.4476e-07 4.68828L-2.04931e-07 3.31172L6.64615 3.31172Z" fill="currentColor"></path>
     </svg>
 );
