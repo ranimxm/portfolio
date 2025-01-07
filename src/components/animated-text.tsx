878faa8cb1a0cb38c7f2115b86c4c4b806b32145
ranim <ranimm01@hotmail.com>
2025-01-07 14:10:19 +0100

    refactor ProjectPage and AnimatedText component to support project links and enhance layout

--- src/components/animated-text.tsx
+++ src/components/animated-text.tsx
@@ -7,16 +7,17 @@ import { ScrollTrigger } from "gsap/ScrollTrigger";
 type AnimatedText = {
     text: string,
     className? : string
+    isLink? : boolean
 }
 
-export const AnimatedText = ({text, className}: AnimatedText) => {
+export const AnimatedText = ({text, className, isLink}: AnimatedText) => {
     const textRef = useRef<HTMLDivElement>(null);
     
     useEffect (() => {
       gsap.registerPlugin(ScrollTrigger);
       if (!textRef.current) return;
 
-      gsap.fromTo(textRef.current.querySelector("p"),
+      gsap.fromTo(textRef.current.querySelector("p, a"),
       { color: "#4f46e5" },
       {
         color: "rgb(37, 37, 37)",  
@@ -36,7 +37,13 @@ export const AnimatedText = ({text, className}: AnimatedText) => {
 
   return (
     <div className={`${className} text`} ref={textRef}>
-      <p>{text}</p>
+      {isLink ? (
+        <a href={text} target="_blank" rel="noopener noreferrer" className="hover-link font-semibold">
+          {text}
+        </a>
+      ) : (
+        <p>{text}</p>
+      )}
     </div>
   )
 }
