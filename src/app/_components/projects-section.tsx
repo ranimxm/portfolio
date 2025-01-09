38afd8e61b5773db7b77196d509d9c5d1e5b3003
ranim <ranimm01@hotmail.com>
2025-01-09 16:45:48 +0100

    enhance project section with hover effects and dynamic background colors

--- src/app/_components/projects-section.tsx
+++ src/app/_components/projects-section.tsx
@@ -20,21 +20,28 @@ export default function ProjectsSection() {
                     <section className="border-borderLine border-r-[1px] border-l-[1px] w-full">
                         <ol className="flex justify-center w-full flex-col">
                             {projects.map((project, index) => (
-                                <li key={project.id}>
-                                    <Link href={`/projects/${project.slug}`} key={project.id}  className={`group hover:cursor-pointer flex flex-col md:flex-row 
-                                        ${index === projects.length -1 ? "border-b-0" : "border-b-[1px]"} border-borderLine h-[80vh] md:h-[50vh] items-center gap-2 p-2 md:p-5`}>
-                                        <figure className=" h-[50vh] md:h-full md:w-[30%] w-full px-[2vw]">
-                                            <img src={project.image} alt={project.title} className="h-full w-auto object-cover"/>
+                                <li key={project.id} 
+                                className="group relative overflow-hidden before:absolute before:top-full before:left-0 before:h-full before:w-full 
+                                before:bg-[var(--bg-color)] before:transition-transform before:duration-500 
+                                hover:cursor-pointer hover:before:-translate-y-full hover:text-white"
+                                style={{
+                                    '--bg-color': project.hoverColor
+                                } as React.CSSProperties}>
+                                    <Link href={`/projects/${project.slug}`} key={project.id}  className={`flex flex-col md:flex-row 
+                                        ${index === projects.length -1 ? "border-b-0" : "border-b-[1px]"} border-borderLine h-[80vh] md:h-[50vh] items-center gap-2 p-2 md:p-5
+                                         `}>
+                                        <figure className=" h-[50vh] z-[30] md:h-full md:w-[30%] w-full px-[2vw] overflow-hidden">
+                                            <img src={project.image} alt={project.title} className="h-full w-auto object-cover transform transition-transform duration-300 group-hover:scale-110"/>
                                         </figure>
-                                        <div className="flex flex-col md:flex-row w-full md:w-[70%] justify-between px-[2vw] md:items-center">
-                                            <figcaption className="md:p-[3em] grow-[1] text-h2 flex flex-col gap-8">
-                                                <AnimatedText text={project.title} />
-                                                <AnimatedText text={project.framework} />
-                                            </figcaption>
-                                            <div className="px-5 md:block hidden">
-                                                <ChevronRight className="text-black group-hover:text-mainTheme group-hover:-translate-x-4 ease-out duration-300"/>
+                                        <figcaption className="flex flex-col md:flex-row w-full md:w-[70%] justify-between px-[2vw] md:items-center">
+                                            <div className="md:p-[3em] grow-[1] text-h2 flex flex-col gap-8 group-hover:-translate-x-4 ease-out duration-300">
+                                                <p>{project.title}</p>
+                                                <p>{project.framework}</p>
                                             </div>
-                                        </div>
+                                            <div className="px-5 md:block hidden">
+                                                <ChevronRight className="text-black group-hover:text-white group-hover:-translate-x-4 ease-out duration-300"/>
+                                            </div>
+                                        </figcaption>
                                     </Link>
                                 </li>
                             ))}
