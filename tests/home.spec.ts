1d690cfbcea7e76687fb1de1afee3337628d357f
ranimm01 <ranimm01@hotmail.com>
2025-04-18 23:38:58 +0200

    Update home.spec.ts

--- tests/home.spec.ts
+++ tests/home.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from "@playwright/test";
 test("home: has title", async ({ page }) => {
   await page.goto("/");
   await page.waitForTimeout(2000);
-  await expect(page.locator("text=Turning pixels into playful")).toBeVisible();
+  await expect(page.locator("text=Building logic,")).toBeVisible();
 });
 
 test("home: click on project-item", async ({ page }) => {
